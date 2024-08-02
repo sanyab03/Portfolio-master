db9a587191fa76d7102dedf5f3059a36017aa5e8
sanyab31 <sanya@grow4tech.com>
2024-08-02 15:37:51 +0530

    Make all the changes

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import editor from "../../Assets/Projects/codeEditor.png";
 import Foodfolio from "../../Assets/Projects/foodfolio.png";
 import EcoEscapes from "../../Assets/Projects/Ecoescapes.png";
 import Craftcuisine from "../../Assets/Projects/Craftcuisine.png";
@@ -32,21 +31,21 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
+              imgPath={EcoEscapes}
+              isBlog={false}
+              title="Eco Escapes"
+              description="EcoEscapes is a vibrant travel website for eco-friendly travelers. It offers an intuitive interface for exploring sustainable destinations, booking green accommodations, and accessing travel guides, ensuring a user-friendly experience."
+              demoLink="https://eco-escapes.vercel.app/"              
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
               imgPath={Craftcuisine}
               isBlog={false}
               title="Craft Cuisine"
               description="
               Craft Cuisine is a dynamic restaurant website with an intuitive interface for exploring menus and making reservations. It showcases high-quality dish images and customer reviews, ensuring an engaging and user-friendly experience for food enthusiasts."
               demoLink="https://craft-cuisine.vercel.app/"              
-            />
-          </Col>
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={EcoEscapes}
-              isBlog={false}
-              title="Eco Escapes"
-              description="EcoEscapes is a vibrant travel website for eco-friendly travelers. It offers an intuitive interface for exploring sustainable destinations, booking green accommodations, and accessing travel guides, ensuring a user-friendly experience."
-              demoLink="https://eco-escapes.vercel.app/"              
             />
           </Col>
         </Row>
